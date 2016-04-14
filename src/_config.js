17a847257e807b3ffa257ba334ec36e66b497765
millsmcilroy <millsmcilroy@gmail.com>
2016-04-14 10:42:19 -0600

    heroku init

--- src/_config.js
+++ src/_config.js
@@ -2,7 +2,9 @@ var config = {};
 
 config.mongoURI = {
   development: 'mongodb://localhost/first-mean-app',
-  test: 'mongodb://localhost/first-mean-app-testing'
+  test: 'mongodb://localhost/first-mean-app-testing',
+  production: process.env.mongoURI
+
 };
 
 module.exports = config;
